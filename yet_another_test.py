3b19eb7f8f57c39f00169d64e61cfa1c60e230d2
Gleb Ignatiev <glebignatieff@gmail.com>
2018-05-13 06:17:49 -0400

    tmp commit

--- yet_another_test.py
+++ yet_another_test.py
@@ -1,134 +1,53 @@
-import os
-import sqlite3
-from collections import OrderedDict
+import pickle
+from math import sqrt
 
-import mmh3
 import numpy as np
-# from sklearn.preprocessing import LabelEncoder
-
-from common import get_unique_api_list
-from common import get_files_paths
+from sklearn.model_selection import train_test_split
 
 
-def to_3dim_24bit(x):
-    x &= 0xffffff
-    ret = []
-    for i in range(3):
-        _8bit = (x >> 8 * (2 - i)) & 0xff
-        ret.append(_8bit)
-    return ret
+with open('dataset/dataset_chunk_0.pkl', 'rb') as f:
+    dataset_benign = pickle.load(f)
 
+with open('dataset/dataset_chunk_5.pkl', 'rb') as f:
+    dataset_malware = pickle.load(f)
 
-def unique(_list, keep_order=False):
-    if keep_order:
-        return list(OrderedDict.fromkeys(_list))
-    else:
-        return np.unique(_list)
+dataset = {}
+dataset['data'] = np.vstack((dataset_benign['data'], dataset_malware['data']))
+dataset['labels'] = np.concatenate((dataset_benign['labels'], dataset_malware['labels']))
+dataset['filenames'] = np.concatenate((dataset_benign['filenames'], dataset_malware['filenames']))
 
+n_channels = 3  # rgb
+n_samples = dataset['data'].shape[0]
+h_w = int(sqrt(dataset['data'].shape[1] / n_channels))  # height = width
+img_size = (h_w, h_w)
 
-def encode_api(api):
-    return mmh3.hash(api) & 0xffffff
+X = dataset['data'].reshape(n_samples, n_channels, *img_size).transpose(0, 2, 3, 1)
+y = dataset['labels']
 
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-def encoding_apis_without_perm_level():
-    unique_apis = get_unique_api_list()
-    encoded_unique_apis = unique([encode_api(api) for api in unique_apis], True)
-    # no_api = len(unique_apis)   # for padding
+with open('train/x_train.pkl', 'wb') as f:
+    pickle.dump(x_train, f)
 
-    # label_encoder = LabelEncoder()
-    # integer_encoded = label_encoder.fit(unique_apis)
+with open('train/x_test.pkl', 'wb') as f:
+    pickle.dump(x_test, f)
 
-    api_seq_files = get_files_paths('api_sequences/')
+with open('train/y_train.pkl', 'wb') as f:
+    pickle.dump(y_train, f)
 
-    apis_vector = []
-    labels = []
-    for file in api_seq_files:
-        with open(file) as f:
-            apis = f.read().strip().split('\n')
-            apis_vector.append([encode_api(api) for api in apis])
-            # apis_vector.append(label_encoder.transform(apis))
-            if 'malware' in file:
-                labels.append(1)
-            else:
-                labels.append(0)
+with open('train/y_test.pkl', 'wb') as f:
+    pickle.dump(y_test, f)
 
-    # API inverse transform example
-    print(apis_vector[0][0], unique_apis[encoded_unique_apis.index(apis_vector[0][0])])
+#########################################################################################################
 
+with open('train/x_train.pkl', 'rb') as f:
+    x_train = pickle.load(f)
 
-##########################################################################################
+with open('train/x_test.pkl', 'rb') as f:
+    x_test = pickle.load(f)
 
-# Permissions
-normal_perms = 'data/permissions/normal.txt'
-signature_perms = 'data/permissions/signature.txt'
-dangerous_perms = 'data/permissions/dangerous.txt'
+with open('train/y_train.pkl', 'rb') as f:
+    y_train = pickle.load(f)
 
-
-# Permission levels (tmp values)
-PERM_PROTECTION_LEVEL = {
-    'UNKNOWN':      0,
-    'NO_LEVEL':     1,
-    'NORMAL':       2,
-    'SIGNATURE':    3,
-    'DANGEROUS':    4
-}
-
-
-AXPLORER_APIS = {'min': 16, 'max': 25}
-PSCOUT_APIS = {'min': 9, 'max': 22}
-
-
-def get_perm_level_dict():
-    ret = {}
-
-    with open(normal_perms) as f:
-        for perm in f.read().strip().split('\n'):
-            ret[perm] = PERM_PROTECTION_LEVEL['NORMAL']
-
-    with open(signature_perms) as f:
-        for perm in f.read().strip().split('\n'):
-            ret[perm] = PERM_PROTECTION_LEVEL['SIGNATURE']
-
-    with open(dangerous_perms) as f:
-        for perm in f.read().strip().split('\n'):
-            ret[perm] = PERM_PROTECTION_LEVEL['DANGEROUS']
-
-    return ret
-
-
-def get_permission_level_by_api(cursor, api, api_level, perm_level_dict):
-    query = "SELECT permission FROM {}_API_{} WHERE api='{}'"
-
-    if api.startswith('Landroid'):
-        max_perm_level = PERM_PROTECTION_LEVEL['NO_LEVEL']
-    else:
-        max_perm_level = PERM_PROTECTION_LEVEL['UNKNOWN']
-
-    # axplorer query
-    if api_level >= AXPLORER_APIS['min'] and api_level <= AXPLORER_APIS['max']:
-        cursor.execute(query.format('AXPLORER', api_level, api))
-        for perm in cursor.fetchall():
-            if perm[0] in perm_level_dict:
-                level = perm_level_dict[perm[0]]
-                if level > max_perm_level:
-                    max_perm_level = level
-
-    # PScout query
-    if api_level >= PSCOUT_APIS['min'] and api_level <= PSCOUT_APIS['max']:
-        cursor.execute(query.format('PSCOUT', api_level, api))
-        for perm in cursor.fetchall():
-            if perm[0] in perm_level_dict:
-                level = perm_level_dict[perm[0]]
-                if level > max_perm_level:
-                    max_perm_level = level
-
-    return max_perm_level
-
-
-perm_level_dict = get_perm_level_dict()
-
-
-with sqlite3.connect('mappings/mapping.db') as conn:
-    cursor = conn.cursor()
-    perm_level = get_permission_level_by_api(cursor, 'Landroid/inputmethodservice/InputMethodService;->clearWallpaper()V', 16, perm_level_dict)
-    print(perm_level)
+with open('train/y_test.pkl', 'rb') as f:
+    y_test = pickle.load(f)
